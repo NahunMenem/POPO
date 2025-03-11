2b16c4ea52ff1732a50161a0b97c2094cf01b7d9
Nahun Menem <86641118+NahunMenem@users.noreply.github.com>
2025-03-10 23:55:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 app.secret_key = 'tu_clave_secreta_aqui'  # Necesario para usar sesiones
 
 # Configuración de la base de datos PostgreSQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:UFkrfqzhyiczVeORiHmZdaLCvcrwNlII@postgres.railway.internal:5432/railway'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
